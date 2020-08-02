d0e297bfcc96c41066bb1fab0bd0143013329427
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-08-02 16:44:37 +1200

    change picture of retail stores to store logo

--- src/components/RetailProductCard.js
+++ src/components/RetailProductCard.js
@@ -9,7 +9,7 @@ class RetailProductCard extends React.Component {
 		return (
 			<div className="card mb-3">
 				<div className="row no-gutters">
-					<div className="col-md-4">
+					<div className="col-md-4" style={{ margin: "auto" }}>
 						<img src={ this.props.imageSrc } className="card-img" alt="image for phone" />
 					</div>
 					<div className="col-md-8">
@@ -17,9 +17,9 @@ class RetailProductCard extends React.Component {
 							<h5 className="card-title">{ this.props.retailName }</h5>
 							<hr/>
 							<div className="row">
-								<div className="col-6">
-									<p className="card-text"><small className="text-muted">{ this.props.productName }</small></p>
-								</div>
+								{/*<div className="col-6">*/}
+								{/*	<p className="card-text"><small className="text-muted">{ this.props.productName }</small></p>*/}
+								{/*</div>*/}
 								<div className="col-6">
 									<p className="card-text"><small className="text-muted">{ this.props.price }</small></p>
 								</div>
--- src/components/SideRetailBar.js
+++ src/components/SideRetailBar.js
@@ -13,29 +13,29 @@ class SideRetailBar extends React.Component {
 				<h1 className="retailTitle"> Retail Prices</h1>
 				<div style={{ overflow: "scroll", height: "100vh" }}>
 					<RetailProductCard
-						imageSrc={ "https://cdn.pricespy.co.nz/product/front/800/4939135.jpg" }
-						retailName={ "Expert Infotech" }
+						imageSrc={ "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/f4966f66724f64a2e9a0f8f937aa8da9" }
+						retailName={ "Harvey Norman" }
 						productName={ "Apple iPhone XR 64GB" }
 						price={ "$1,063.00" }
 						rating={ "★★★★★" }
 					/>
 
 					<RetailProductCard
-						imageSrc={ "https://cdn.pricespy.co.nz/product/standard/800/4939135.jpg" }
-						retailName={ "JB Hi-Fi" }
+						imageSrc={ "https://i.imgur.com/Hp5gqEB.png" }
+						retailName={ "Noel Leeming" }
 						productName={ "APPLE Apple iPhone XR 64GB (Black)" }
 						price={ "$1,149.00" }
 						rating={ "★★★☆☆" }
 					/>
 					<RetailProductCard
-						imageSrc={ "https://cdn.pricespy.co.nz/product/front/800/4471962.jpg" }
-						retailName={ "PETECH" }
+						imageSrc={ "https://i.ytimg.com/vi/g9zecfafqyU/maxresdefault.jpg" }
+						retailName={ "PB TECH" }
 						productName={ "Apple iPhone XR 64GB (Black)" }
 						price={ "$1,149.00" }
 						rating={ "★★★☆☆" }
 					/>
 					<RetailProductCard
-						imageSrc={ "https://cdn.pricespy.co.nz/product/right/800/4939135.jpg" }
+						imageSrc={ "https://scontent.fakl7-1.fna.fbcdn.net/v/t1.0-9/1001650_574460042604594_220445246_n.png?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=kQVcZOIU8eYAX8GEpqD&_nc_oc=AQkbrtZZyLJ2aJeIpKX5-6LqoiyCmlRswjx3i4K3Ntl91LPCusjjfB4Eh1JfIkItSv8&_nc_ht=scontent.fakl7-1.fna&oh=65b04050d1d0be1604e7cb858459bd16&oe=5F4A54EB" }
 						retailName={ "eSquare" }
 						productName={ "iPhone XR" }
 						price={ "$1,669.00" }
